6f1c84e3349d0d0eefc1a8126442c665588b1c47
ShinjungOh <tlswjd9260@gmail.com>
2023-01-11 18:51:50 +0900

    feat: NavBar 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Page } from './ui/components/layout';
 import GlobalStyles from './ui/core/GlobalStyles';
+import SearchPage from './ui/pages/SearchPage';
+import NavBar from './ui/components/navbar/NavBar';
 
 const App = () => (
   <>
+    <NavBar />
     <Page>
+      <SearchPage />
     </Page>
     <GlobalStyles />
   </>
--- src/ui/components/layout/Layout.tsx
+++ src/ui/components/layout/Layout.tsx
@@ -16,8 +16,5 @@ export default Layout;
 const Container = styled.div`
   width: 100%;
   height: 462px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  background-color: #caeaff;
 `;
